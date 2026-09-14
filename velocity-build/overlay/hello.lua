9875f7864e3a540d40f55f44c27cb3677c9c4ed5
MCB <a0989217838@gmail.com>
2026-09-14 10:10:03 +0800

    Rebrand Lua example to MCB

--- velocity-build/overlay/hello.lua
+++ velocity-build/overlay/hello.lua
@@ -1,11 +1,11 @@
-velocity.log("hello.lua loaded")
+mcb.log("hello.lua loaded")
 local elapsed = 0
-function on_load() velocity.log("on_load") end
+function on_load() mcb.log("on_load") end
 function on_frame(dt)
   elapsed = elapsed + dt
   if elapsed >= 10 then
     elapsed = 0
-    velocity.log("alive " .. tostring(velocity.now_ms()))
+    mcb.log("alive " .. tostring(mcb.now_ms()))
   end
 end
-function on_unload() velocity.log("on_unload") end
+function on_unload() mcb.log("on_unload") end
