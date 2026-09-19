1a92b532cd786c87d731c427f3f757dbea6afb2d
MCB <a0989217838@gmail.com>
2026-09-20 01:12:06 +0800

    MCB Nix: define guarded paint render scope

--- velocity-build/overlay/nix_render_bridge.hpp
+++ velocity-build/overlay/nix_render_bridge.hpp
@@ -9,4 +9,6 @@ namespace scripting::nix_native
     bool install_render_api(luajit_api& api, lua_State* state, std::string& error);
     void detach_render_api(lua_State* state);
     void advance_render_frame();
+    void begin_render_scope(lua_State* state);
+    void end_render_scope(lua_State* state);
 }
